4f5f8c613971f8ddcd60e228772b40de825d9909
seloukrtv-KKKSSS <seloukrtv@gmail.com>
2026-07-14 21:53:04 +0300

    Fix: match ksliga.com - env vars, CSS colors, layout metadata, GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'KS LIGA',
+  description: 'KS LIGA',
+  generator: 'v0.dev',
 }
 
 export default function RootLayout({
@@ -17,6 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PBKCJ68RYL"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PBKCJ68RYL');
+            `}} />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
@@ -29,3 +36,4 @@ html {
     </html>
   )
 }
+
